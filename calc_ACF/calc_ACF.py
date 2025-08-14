befd99d731ec5e79729db34ffd5f3c392930f50e
ananbaobeichicun <47203967+ananbaobeichicun@users.noreply.github.com>
2025-08-14 10:57:30 +0800

    统一代码格式，用双引号代替单引号

--- calc_ACF/calc_ACF.py
+++ calc_ACF/calc_ACF.py
@@ -6,8 +6,8 @@ from statsmodels.tsa.stattools import acf
 import matplotlib.pyplot as plt
 
 def set_font():
-    plt.rcParams['font.sans-serif'] = ['SimHei']  # 使用黑体
-    plt.rcParams['axes.unicode_minus'] = False    # 解决负号显示问题
+    plt.rcParams["font.sans-serif"] = ["SimHei"]  # 使用黑体
+    plt.rcParams["axes.unicode_minus"] = False    # 解决负号显示问题
 
 def find_max_acf_lag(series, l, r):
     """
@@ -41,36 +41,36 @@ set_font()
 
 print("正在读取数据 ...")
 print(f"当前工作目录: {os.getcwd()}")
-df = pd.read_excel('data_process/profit_filtered.xlsx')
+df = pd.read_excel("data_process/profit_filtered.xlsx")
 
 print("数据预处理 ...")
-# df['销售日期'] = pd.to_datetime(df['销售日期'], errors='coerce')
-df['销售日期'] = pd.to_datetime(df['销售日期'])
-df.set_index('销售日期', inplace=True)
-df['销量(千克)'] = (df['销量(千克)'] - df['销量(千克)'].mean()) / df['销量(千克)'].std()
+# df["销售日期"] = pd.to_datetime(df["销售日期"], errors="coerce")
+df["销售日期"] = pd.to_datetime(df["销售日期"])
+df.set_index("销售日期", inplace=True)
+df["销量(千克)"] = (df["销量(千克)"] - df["销量(千克)"].mean()) / df["销量(千克)"].std()
 
 print("数据读取完成。开始计算自相关函数(ACF) ...")
 # 绘制自相关图
 plt.figure(figsize=(60, 30))
 
-quarter_len = len(df['销量(千克)']) // 4
-plot_acf(df['销量(千克)'], lags=quarter_len, alpha=0.05)  # alpha=0.05表示95%置信区间
+quarter_len = len(df["销量(千克)"]) // 4
+plot_acf(df["销量(千克)"], lags=quarter_len, alpha=0.05)  # alpha=0.05表示95%置信区间
 plt.title("自相关函数(ACF)")
-plt.xlabel('滞后阶数')
-plt.ylabel('自相关系数')
+plt.xlabel("滞后阶数")
+plt.ylabel("自相关系数")
 plt.grid(True)
 # plt.show()
 
 print("自相关函数(ACF)计算绘制图形完成，正在保存图形 ...")
-plt.savefig('ACF_plot.png', dpi=300, bbox_inches='tight', pad_inches=0.1)
-plt.savefig('ACF_plot.svg', bbox_inches='tight', pad_inches=0.1)
+plt.savefig("ACF_plot.png", dpi=300, bbox_inches="tight", pad_inches=0.1)
+plt.savefig("ACF_plot.svg", bbox_inches="tight", pad_inches=0.1)
 
 plt.xlim(110000, 120000)
-plt.savefig('ACF_plot_zoomed.png', dpi=300, bbox_inches='tight', pad_inches=0.1)
-plt.savefig('ACF_plot_zoomed.svg', bbox_inches='tight', pad_inches=0.1)
+plt.savefig("ACF_plot_zoomed.png", dpi=300, bbox_inches="tight", pad_inches=0.1)
+plt.savefig("ACF_plot_zoomed.svg", bbox_inches="tight", pad_inches=0.1)
 
 print("图形保存完成。 开始查找最大 ACF 值 ...")
-max_index, max_acf = find_max_acf_lag(df['销量(千克)'], 110000, 120000)
+max_index, max_acf = find_max_acf_lag(df["销量(千克)"], 110000, 120000)
 print(f"最大 ACF 值 {max_acf:.4f} 对应的滞后阶数为 {max_index}。") # 输出: 最大 ACF 值 0.0588 对应的滞后阶数为 115951。
 
 print("结束")
--- data_process/data_process.py
+++ data_process/data_process.py
@@ -3,12 +3,12 @@ import numpy as np
 import xlwings as xw
 
 print("正在读取附录2 ...")
-with xw.Book('附件2.xlsx') as book1:
+with xw.Book("附件2.xlsx") as book1:
     sheet1 = book1.sheets[0]
     df1 = sheet1.used_range.options(pd.DataFrame, header=True, index=False).value
 
     print("正在读取附录3 ...")
-    with xw.Book('附件3.xlsx') as book2:
+    with xw.Book("附件3.xlsx") as book2:
         sheet2 = book2.sheets[0]
         df2 = sheet2.used_range.options(pd.DataFrame, header=True, index=False).value
 
@@ -16,15 +16,15 @@ with xw.Book('附件2.xlsx') as book1:
         for idx, row in df1[:].iterrows():
             if idx % 1000 == 0:
                 print(f"Processing row {idx + 1} of {len(df1)}, {idx / len(df1) * 100:.2f}% complete")
-            product_code = row['单品编码']
-            wholesale_price = df2.loc[df2['单品编码'] == product_code, '批发价格(元/千克)']
+            product_code = row["单品编码"]
+            wholesale_price = df2.loc[df2["单品编码"] == product_code, "批发价格(元/千克)"]
             if not wholesale_price.empty:
-                profit = row['销售单价(元/千克)'] - wholesale_price.values[0]
-                df1.at[idx, '利润'] = profit
+                profit = row["销售单价(元/千克)"] - wholesale_price.values[0]
+                df1.at[idx, "利润"] = profit
 
     print("正在过滤异常值 ...")
 
-    data_series = df1['利润']
+    data_series = df1["利润"]
     mean_val = data_series.mean()
     std_val = data_series.std()
 
@@ -39,9 +39,9 @@ with xw.Book('附件2.xlsx') as book1:
     print(f"被删除的行号: {list(removed_indices)}")
 
     # 将利润写入H列
-    sheet1.range('H1').value = '利润'
-    sheet1.range('H2').options(index=False, header=False).value = df1['利润'].values.reshape(-1, 1)
-    sheet1.range('H:H').api.HorizontalAlignment = -4108  # -4108 代表xlHAlignCenter
+    sheet1.range("H1").value = "利润"
+    sheet1.range("H2").options(index=False, header=False).value = df1["利润"].values.reshape(-1, 1)
+    sheet1.range("H:H").api.HorizontalAlignment = -4108  # -4108 代表xlHAlignCenter
 
     # 删除异常值对应的行（Excel中行号需加2，因为DataFrame索引从0开始且Excel有表头）
     for idx in removed_indices:
@@ -49,6 +49,6 @@ with xw.Book('附件2.xlsx') as book1:
         sheet1.range(f"{excel_row}:{excel_row}").delete()
 
     # 保存结果
-    book1.save('profit_filtered.xlsx')
+    book1.save("profit_filtered.xlsx")
 
-    print("数据处理完成。结果已保存到 'profit_filtered.xlsx'。")
+    print('数据处理完成。结果已保存到 "profit_filtered.xlsx"。')
